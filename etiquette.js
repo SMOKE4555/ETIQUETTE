2e996860aa70acdf97c893f24da61ee63e632bcf
SMOKE4555 <albertkunihira8@gmail.com>
2025-07-28 21:25:50 +0300

    UPDATED

--- etiquette.js
+++ etiquette.js
@@ -33,3 +33,27 @@ offers.forEach(offer => {
     arrow.style.transform = 'translateX(0)';
   });
 });
+
+
+// ...existing code...
+
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#customers-testimonials').owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    responsive: {
+      0: { items: 1 },
+      768: { items: 2 },
+      1170: { items: 3 }
+    }
+  });
+});
+
+
